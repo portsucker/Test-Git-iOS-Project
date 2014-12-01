00b657bf91b06eb98068365fd8d59629d9effd13
portsucker <kush@Kushs-MacBook-Pro.local>
2014-12-01 12:40:56 +0800

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Kush Shah on 1/12/2014.
+//  Copyright (c) 2014 Kush Shah. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Kush Shah on 1/12/2014.
+//  Copyright (c) 2014 Kush Shah. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
